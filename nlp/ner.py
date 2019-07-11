28fec136c12e7567cc978585b439be2f17912d7a
chriscross00 <christopher.chan2014@gmail.com>
2019-07-11 01:06:14 -0700

    added reading links

--- nlp/ner.py
+++ nlp/ner.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python
+
 import spacy
 
 nlp = spacy.load('en')
-doc = nlp('hello world!')
+doc = nlp('hello world! this is a test of spacy')
 
-for token in doc:
-    print('"' + token.text + '"')
+print([(token.text, token.tag_) for token in doc])
