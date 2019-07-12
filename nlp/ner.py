f1dcdc5097a9cff06d7eb83729707a4e8a9ce3f5
chriscross00 <christopher.chan2014@gmail.com>
2019-07-11 23:57:50 -0700

    practice ner

--- nlp/ner.py
+++ nlp/ner.py
@@ -1,8 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+from bs4 import BeautifulSoup
+
+import re
+import requests
 import spacy
 
-nlp = spacy.load('en')
-doc = nlp('hello world! this is a test of spacy')
 
-print([(token.text, token.tag_) for token in doc])
+def main():
+    nlp = spacy.load('en')
+    ny_bb = url_to_string('https://www.nytimes.com/2018/08/13/us/politics/peter-strzok-fired-fbi.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=first-column-region&region=top-news&WT.nav=top-news')
+    article = nlp(ny_bb)
+
+    print(len(article.ents))
+
+
+def url_to_string(url):
+
+    res = requests.get(url)
+    html = res.text
+    soup = BeautifulSoup(html, 'html5lib')
+
+    for script in soup(['script', 'style', 'aside']):
+        script.extract()
+
+    return ' '.join(re.split(r'[\n\t]+', soup.get_text()))
+
+
+if __name__ == 'main':
+    main()
