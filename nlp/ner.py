bb1d64b8bf81efe33e718ac8ad72eb3f6eff29e4
chriscross00 <christopher.chan2014@gmail.com>
2019-07-12 00:51:12 -0700

    rendering ner

--- nlp/ner.py
+++ nlp/ner.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
+from collections import Counter
+from spacy import displacy
 
 import re
 import requests
 import spacy
 
 
-def main():
-    nlp = spacy.load('en')
-    ny_bb = url_to_string('https://www.nytimes.com/2018/08/13/us/politics/peter-strzok-fired-fbi.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=first-column-region&region=top-news&WT.nav=top-news')
-    article = nlp(ny_bb)
+# globals
+NLP = spacy.load('en')
 
-    print(len(article.ents))
+
+def main():
+    ny_bb = url_to_string('https://www.nytimes.com/2018/08/13/us/politics/peter-strzok-fired-fbi.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=first-column-region&region=top-news&WT.nav=top-news')
+    article = NLP(ny_bb)
+
+    displacy.render(NLP(str(sentences[19])), jupyter=True, style='ent')
 
 
 def url_to_string(url):
 
     res = requests.get(url)
     html = res.text
-    soup = BeautifulSoup(html, 'html5lib')
+    soup = BeautifulSoup(html, 'html.parser')
 
     for script in soup(['script', 'style', 'aside']):
         script.extract()
@@ -27,5 +32,21 @@ def url_to_string(url):
     return ' '.join(re.split(r'[\n\t]+', soup.get_text()))
 
 
-if __name__ == 'main':
+def tokenize(text):
+
+    labels = [x.label_ for x in text.ents]
+    terms = [x.text for x in text.ents]
+
+    return Counter(labels), Counter(terms).most_common(5)
+
+
+def display_text(text):
+
+    sentences = [x for x in text.sents]
+
+    return sentences
+    #displacy.render(NLP(str(sentences[19])), jupyter=True, style='ent')
+
+
+if __name__ == '__main__':
     main()
