44658841bd1ff8347e6ca711ba3c6c3df88575cb
chriscross00 <christopher.chan2014@gmail.com>
2019-07-12 23:25:45 -0700

    added logging

--- nlp/ner.py
+++ nlp/ner.py
@@ -3,6 +3,7 @@
 from bs4 import BeautifulSoup
 from collections import Counter
 
+import datetime
 import logging
 import re
 import requests
@@ -14,14 +15,21 @@ NLP = spacy.load('en')
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(filename='ner.log', level=logging.DEBUG)
+
+    logging.info(f'{datetime.datetime.now()} Running main')
+
     ny_bb = url_to_string('https://www.nytimes.com/2018/08/13/us/politics/peter-strzok-fired-fbi.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=first-column-region&region=top-news&WT.nav=top-news')
     article = NLP(ny_bb)
 
     print(tokenize(article))
 
+    logging.info(f'{datetime.datetime.now()} End main')
+
 
 def url_to_string(url):
+
+    logging.info('Begin requests')
 
     res = requests.get(url)
     html = res.text
@@ -30,6 +38,8 @@ def url_to_string(url):
     for script in soup(['script', 'style', 'aside']):
         script.extract()
 
+    logging.info('End requests')
+
     return ' '.join(re.split(r'[\n\t]+', soup.get_text()))
 
 
@@ -37,6 +47,8 @@ def tokenize(text):
 
     labels = [x.label_ for x in text.ents]
     terms = [x.text for x in text.ents]
+
+    logging.info(f'Tokenizing to {labels} and {terms}')
 
     return Counter(labels), Counter(terms).most_common(5)
 
