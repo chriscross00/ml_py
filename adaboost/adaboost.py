cb215a45e564ccde29c1d220ab9718ed5d14c535
chriscross00 <christopher.chan2014@gmail.com>
2019-07-19 00:56:51 -0700

    created clf func

--- adaboost/adaboost.py
+++ adaboost/adaboost.py
@@ -1,4 +1,31 @@
 #! /usr/bin/env python3
 
+from sklearn.datasets import load_wine
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import train_test_split
 
-print('hello world')
+def load_data():
+	wine = load_wine()
+
+	features, target = wine.data, wine.target
+	X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=0)
+
+	return X_train, X_test, y_train, y_test
+
+
+def a_boost(X_tr, X_te, y_tr, y_te):
+	clf = AdaBoostClassifier(n_estimators=25,
+							learning_rate=0.1,
+							random_state=0)
+
+	clf.fit(X_tr, y_tr)
+	pred = clf.predict(X_te)
+	score = accuracy_Score
+
+def main():
+
+	print('Running model')
+
+
+if __name__ == '__main__':
+	main()
--- adaboost/notes.md
+++ adaboost/notes.md
@@ -1,3 +1,5 @@
 https://www.joshkel.com/2017/05/03/python-2-3-shebangs-and-launchers/
 
 https://stackoverflow.com/questions/27494758/how-do-i-make-a-python-script-executable
+
+https://askubuntu.com/questions/320632/why-do-i-need-to-type-before-executing-a-program-in-the-current-directory
