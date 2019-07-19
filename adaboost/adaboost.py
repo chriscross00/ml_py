a26bdd98431e1804c3ca0e38a8afe50b560ff5a7
Christopher Chan <christopher.chan2014@gmail.com>
2019-07-19 16:21:00 -0700

    finished mode;

--- adaboost/adaboost.py
+++ adaboost/adaboost.py
@@ -1,8 +1,9 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python
 
 from sklearn.datasets import load_wine
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 def load_data():
 	wine = load_wine()
@@ -14,17 +15,27 @@ def load_data():
 
 
 def a_boost(X_tr, X_te, y_tr, y_te):
-	clf = AdaBoostClassifier(n_estimators=25,
-							learning_rate=0.1,
+	clf = AdaBoostClassifier(n_estimators=50,
+							learning_rate=0.01,
 							random_state=0)
 
 	clf.fit(X_tr, y_tr)
 	pred = clf.predict(X_te)
-	score = accuracy_Score
+	score = metrics.accuracy_score(y_te, pred)
+	f1 = metrics.f1_score(y_te, pred, average='micro')
+	
+	print(f'Standard score: {score}')
+	print(f'f1 score: {f1}')
+	
 
 def main():
 
 	print('Running model')
+	
+	X_train, X_test, y_train, y_test = load_data()
+	a_boost(X_train, X_test, y_train, y_test)
+	
+	print('Done!')
 
 
 if __name__ == '__main__':
